b2d64cf031d1684bcbbe8ec1d795cb66196b556a
Niklas Kuusisto <niklas.kuusisto95@gmail.com>
2018-12-12 16:38:47 +0200

    Update api.py
    moiii

--- Back-end/api/api.py
+++ Back-end/api/api.py
@@ -41,8 +41,8 @@ app.config['SECRET_KEY'] = 'super-secret'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'amir.noreply@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Kirekhar@yahoo'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 
 mail = Mail(app)
 # Setup Flask-Security
